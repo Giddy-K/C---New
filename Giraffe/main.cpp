532877659aa3c5d8e815d411b00a16a7f0cebabb
Gidzzy <gideonvertox@gmail.com>
2022-12-09 23:19:19 +0300

    cpp beginer projects

---
+++ Giraffe/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName;
+    int characterAge;
+    characterName =  "Collins";
+    characterAge = 90;
+
+    cout << "There was once a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    
+
+    characterName =  "Tomz";
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not liked being " <<characterAge << endl;
+    return 0;
+}
